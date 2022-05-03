fef3b67742eea74afb48843d8676e0e1fad2e866
Spencer Loveridge <spencerloveridge@gmail.com>
2022-05-03 21:06:51 +0100

    Updating init script

---
+++ README-replace.md
@@ -0,0 +1,28 @@
+# react-springboot-appengine-template
+
+This is an auto-generated prject template.
+
+## Build
+To build this project run the following from the command line
+```shell
+mvn clean install
+```
+
+## Running the application
+Once built run the following:
+```shell
+java -jar -jar ms/target/react-springboot-appengine-template-*.jar
+```
+Once started you should be able to view the following:
+* swagger api documentation: [http://localhost:8080/app/swagger-ui/html](http://localhost:8080/app/swagger-ui.html)
+* swagger api definition json: [http://localhost:8080/app/v2/api-docs](http://localhost:8080/app/v2/api-docs)
+
+## Running the UI
+```shell
+cd builtin npm run storybook
+```
+After a short while you should see a browser window open displaying a storybook app that allows you to test the UI components
+
+![img.png](https://preview.redd.it/nate1bd6wzxz.jpg?auto=webp&s=c155ebd29641ac27a48b8a5a0b38c670467e9a02)
+
+_Good luck. We're all counting on you_
--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# find_and_replace.sh
+#################################################################
+# This is an initialisation script that should be run once only
+# when you first clone the project. It performs the following steps
+#
+# 1. Create a README
+# 2. Sets up the groupIds and artifact ids
+# 3. Removes this file
+# 4. Initialises git
+#################################################################
 
 export existingGroupId=com\.example
 export existingArtifactId=react-springboot-appengine-template
@@ -10,16 +18,27 @@ read groupId
 echo "Artifact Id?"
 read artifactId
 
-echo "Replacing all occurences of $existingGroupId with $groupId in files"
+echo "Creating README"
+mv ./README-replace.md ./README.md
+
+echo "Replacing all occurrences of $existingGroupId with $groupId in files"
 find . -type f -name "pom.xml" -exec sed -i'' -e 's/'$existingGroupId'/'$groupId'/g' {} +
 find . -type f -name "*.json" -exec sed -i'' -e 's/'$existingGroupId'/'$groupId'/g' {} +
 find . -type f -name "*.java" -exec sed -i'' -e 's/'$existingGroupId'/'$groupId'/g' {} +
 find . -type f -name "logback.xml" -exec sed -i'' -e 's/'$existingGroupId'/'$groupId'/g' {} +
 
-echo "Replacing all occurences of $existingArtifactId with $artifactId in files"
+echo "Replacing all occurrences of $existingArtifactId with $artifactId in files"
 find . -type f -name "pom.xml" -exec sed -i'' -e 's/'$existingArtifactId'/'$artifactId'/g' {} +
 find . -type f -name "*.json" -exec sed -i'' -e 's/'$existingArtifactId'/'$artifactId'/g' {} +
 find . -type f -name "*.tsx" -exec sed -i'' -e 's/'$existingArtifactId'/'$artifactId'/g' {} +
 find . -type f -name "*.ts" -exec sed -i'' -e 's/'$existingArtifactId'/'$artifactId'/g' {} +
 find . -type f -name "*.js" -exec sed -i'' -e 's/'$existingArtifactId'/'$artifactId'/g' {} +
 find . -type f -name "*.ini" -exec sed -i'' -e 's/'$existingArtifactId'/'$artifactId'/g' {} +
+find . -type f -name "*.md" -exec sed -i'' -e 's/'$existingArtifactId'/'$artifactId'/g' {} +
+
+echo "Setting up git project"
+rm init.sh
+rm -rf .git
+git init
+git add .
+git commit -m "Initialised $artifactId"
